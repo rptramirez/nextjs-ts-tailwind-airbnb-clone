e89024a4705efe7e113a3511adb23af5cc2d98bf
Ronald Ramirez <ronald.ramirez@ewave.com>
2021-11-26 04:30:55 +0800

    initial commit for mini mvp

---
+++ src/components/MediumCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Image from 'next/image';
+
+function MediumCard({ img, title }: { img: string; title: string }) {
+  return (
+    <div className="cursor-pointer hover:scale-105 transform transition duration-300 ease-out">
+      <div className="relative h-80 w-80">
+        <Image
+          alt="MediumCard Image"
+          src={img}
+          layout="fill"
+          className="rounded-xl"
+        />
+      </div>
+      <h3 className="text-2xl mt-3">{title}</h3>
+    </div>
+  );
+}
+
+export default MediumCard;
---
+++ src/pages/index.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import LargeCard from '../components/LargeCard';
+import MediumCard from '../components/MediumCard';
+import SmallCard from '../components/SmallCard';
+
+const Index = ({
+  exploreData,
+  cardsData,
+}: {
+  exploreData: any;
+  cardsData: any;
+}) => {
+  return (
+    <>
+      <Header />
+      <Banner />
+
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section>
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          {/* Pull some data from a server - API endpoints */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+            {exploreData?.map(
+              ({
+                img,
+                distance,
+                location,
+              }: {
+                img: string;
+                distance: number;
+                location: string;
+              }) => (
+                <SmallCard
+                  key={img}
+                  img={img}
+                  distance={distance}
+                  location={location}
+                />
+              )
+            )}
+          </div>
+        </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(
+              ({ img, title }: { img: string; title: string }) => (
+                <MediumCard key={img} img={img} title={title} />
+              )
+            )}
+          </div>
+        </section>
+
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlists curated by Airbnb."
+          buttonText="Get Inspired"
+        />
+      </main>
+
+      <Footer />
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const exploreData: Promise<object> = await fetch(
+    'https://links.papareact.com/pyp'
+  ).then((res) => res.json());
+
+  const cardsData = await fetch('https://links.papareact.com/zp1').then(
+    (res) => {
+      return res.json();
+    }
+  );
+
+  return {
+    props: {
+      exploreData,
+      cardsData,
+    },
+  };
+}
+
+export default Index;
